b7aa1d09af0f0d4e611e82ddffe8280569a12b51
gabrielmoreira-dev <g.alves.m.2008@gmail.com>
2024-03-17 20:56:52 -0300

    Parte 1 - Criação de serviço

---
+++ app/src/main/java/br/edu/ifsp/scl/sdm/entityservicecommunication/InterEntityCommunication.kt
@@ -0,0 +1,7 @@
+package br.edu.ifsp.scl.sdm.entityservicecommunication
+
+import androidx.lifecycle.MutableLiveData
+
+object InterEntityCommunication {
+    val value: MutableLiveData<Int> = MutableLiveData()
+}
